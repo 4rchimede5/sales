666f7f5aa1029329a2ff1f55deb6f8e527dc1d06
4rchimede5 <116129185+4rchimede5@users.noreply.github.com>
2023-03-21 14:36:33 -0500

    Add files via upload

--- inst.py
+++ inst.py
@@ -5,7 +5,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-def login(broswer,user,pasw):
+host='https://www.instagram.com/'
+
+def login(browser,user,pasw):
 	browser.find_element_by_name('username').send_keys(user)
 	browser.find_element_by_name('password').send_keys(pasw)
 	browser.find_element_by_name('password').submit()
@@ -17,41 +19,68 @@ def indicators(browser):
 	following=data[2]
 	return posts,followers,following
 
-def followers(browser,followers_link,num_followers):
-	followers_link.click()
+def followers(browser,num_followers,person_type):
 	time.sleep(3)
 	exit=browser.find_element_by_class_name('_ac7b._ac7d')
 
-	bubble=browser.find_element_by_class_name('_aano')
+	popup=browser.find_element_by_class_name('_aano')
+	
 	for i in range(int(num_followers)//5):
-		bubble.send_keys(Keys.END)
+		popup.send_keys(Keys.END)
 		time.sleep(1)
+	
 	data=browser.find_elements_by_class_name('x1yutycm')
+	
 	if int(num_followers) == len(data):
-		print('all followers accounted')
+		print('all %s accounted' % person_type)
 	else:
 		missing = int(num_followers) - len(data)
-		print("%d followers are missing"% missing)
-	return data	
-
-def clean_follows(follows):
-	people={}
-	for i in follows:
-		info=i.text
-		info=info.split('\n')
-		handle=info[0]
-		name=info[1]
-		people[handle]=name
-	return people
+		print("%d %s are missing" % (missing,person_type))
+		followers(browser,num_followers,person_type)
 	
-		
+	exit.click()
+	clean_follows(data,person_type)	
+
+def clean_follows(data, person_type):
+	people={}
+	for i in data:
+		person=i.text
+		person=person.split('\n')
+		handle=person[0]
+		name=person[1]
+		people[handle]=name
+
+	try:
+		output = open(person_type,'r')
+	except:
+		file=open(person_type,'w')
+		file.close()
+		output=open(person_type,'r')
+	finally:
+		current_follower=output.read()
+		current_follower=current_follower.split('\n')
+		output.close()
+
+	output=open(person_type,'a')
+
+	for i in people:
+		if i in current_follower:
+			print('%s is already accounted'% i)
+		else:
+			output.write('%s \t %s\n'% (i,handle_name[i])
+
+def following(browser, fo)"
+	#
+###
+#
+#
 
 browser=webdriver.Firefox()
 browser.get('https://www.instagram.com/accounts/login/')
 
 browser.implicitly_wait(30)
-user=''
-pasw=''
+user='zforero@live.com'
+pasw='Followfollow5!'
 
 login(browser,user,pasw)
 
@@ -76,35 +105,16 @@ profile=buttons[8]
 profile.click()
 
 pro_ind={}
-
 posts_link,followers_link,following_link=indicators(browser)
-
 #print(post_link.text, followers_link.text, following_link.text)
 pro_ind['posts']=posts_link.text
 pro_ind['followers']=followers_link.text
 pro_ind['following']=following_link.text
+pro_ind['user']=browser.find_by_element_tag_name('h2').text	
 
-follows=followers(browser, followers_link,pro_ind['followers'])
-
-handle_name=clean_follows(follows)
-
-try:
-	follower_out = open('followers','r')
-except:
-	file=open('followers','w')
-	file.close()
-	follower_out=open('followers','r')
-finally:
-	current_follower=follower_out.read()
-	current_follower=current_follower.split('\n')
-	follower_out.close()
-
-follower_out=open('followers','a')
-
-for i in handle_name:
-	if i in current_follower:
-		print('%s is already accounted'% i)
-	else:
-		follower_out.write('%s	%s\n'% (i,handle_name[i]))
-
-#follow=following(browser)
+browser.get(host+pro_ind['user']+'/followers/')
+follows=followers(browser, pro_ind['followers'],'followers')
+clean_follows(follows)
+browser.get(host+pro_ind['user']+'/following/')
+follow=following(browser,pro_ind['following'],'following')
+clean_follows(follow)
